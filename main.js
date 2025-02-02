43a40e3a1ac4ff598d0dd579090fa9642da9c40f
AndreaFroncillo <andreafroncillo0@gmail.com>
2025-02-02 13:30:44 +0100

    Modificato esercizio e terminato di nuovo

---
+++ main.js
@@ -0,0 +1,12 @@
+let a = parseInt(prompt('Inserisci il primo numero'));
+let b = parseInt(prompt('Inserisci il secondo numero'));
+
+let somma = a + b;
+let dif = a - b;
+let mol = a * b;
+let div = a / b;
+let pot = a ** b;
+
+console.log('Con i numeri d ate scelti, i risultati delle varie operazioni sono: ');
+
+console.log(`somma(${somma}), sottrazione(${dif}), moltiplicazione(${mol}), divisione(${div}), potenza(${pot}).`);
